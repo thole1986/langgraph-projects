a0525734f876bcee78f4c7d16030a14de37a3d77
ThoLe <tholhlearning@gmail.com>
2026-06-05 17:02:19 +0700

    update import

--- agentic-rag/graph/nodes/web_search.py
+++ agentic-rag/graph/nodes/web_search.py
@@ -1,7 +1,7 @@
 from typing import Any, Dict
 
 from dotenv import load_dotenv
-from langchain.schema import Document
+from langchain_core.documents import Document
 from langchain_tavily import TavilySearch
 
 from graph.state import GraphState
